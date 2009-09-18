95449bce117a358118b8a7cf6f1e068cb5a4b065
Taneli Vähäkangas <taneli.vahakangas@cs.helsinki.fi>
2009-09-18 16:36:42 +0300

    Add defaults

--- imap2atom.py
+++ imap2atom.py
@@ -3,22 +3,40 @@ import imaplib
 import getpass
 import email, email.Utils
 #import datetime
+import socket
 import time
-import cElementTree as ET
+import xml.etree.cElementTree as ET
 
-IMAPSERVER = os.getenv("IMAPSERVER")
-IMAPUSERNAME = os.getenv("IMAPUSERNAME")
-IMAPPASSWORD = os.getenv("IMAPPASSWORD")
+CONFIG = {
+	'IMAPSERVER': "imap." + ".".join(socket.getfqdn().split(".")[1:]),
+	'IMAPUSERNAME': os.getlogin(),
+	'FOLDERNAME': "Inbox",
+	'OUTPUTFILE': "imap.xml",
+	'DAYS': "14",
+	'DEBUG': 0
+	}
+
+def readconfig(fname):
+	for line in file(fname, "r").read().splitlines():
+		curline = line.strip()
+		if curline.startswith("#"):
+			continue
+		k, v = map(lambda x: x.strip(), curline.split("="))
+		if CONFIG['DEBUG']:
+			print "DBG: Adding key %s with value %s" % (k, v)
+		CONFIG[k] = v
 
 def sort(L):
 	L.sort()
 	return L
 
 def gethdrs(fieldspec):
+	"""Get IMAP headers according to fieldspec, which must follow
+	RFC 3501 SEARCH specification."""
 	retval = list()
-	mbox = imaplib.IMAP4(IMAPSERVER)
-	mbox.login(IMAPUSERNAME, IMAPPASSWORD)
-	mbox.select("INBOX", True)
+	mbox = imaplib.IMAP4(CONFIG['IMAPSERVER'])
+	mbox.login(CONFIG['IMAPUSERNAME'], CONFIG['IMAPPASSWORD'])
+	mbox.select(CONFIG['FOLDERNAME'], True)
 	typ, nums = mbox.search(None, fieldspec)
 	print "INFO: fetching %d headers." % len(nums[0].split())
 	for num in nums[0].split():
@@ -39,7 +57,7 @@ def generate_feed(feed, hdrs):
 	# <link href="imap://server/INBOX;UID" rel="alternate"/>
 		link = ET.SubElement(elem, "link")
 		link.attrib['rel'] = "alternate"
-		link.attrib['href'] = "imap://%s/INBOX;UID=%s" % (IMAPSERVER, hdr[0])
+		link.attrib['href'] = "imap://%s/%s;UID=%s" % (CONFIG['IMAPSERVER'], CONFIG['FOLDERNAME'], hdr[0])
 
 	# <id>UID</ID>
 		ET.SubElement(elem, "id").text = phdr['Message-ID'].strip("<>")
@@ -59,22 +77,28 @@ def generate_feed(feed, hdrs):
 		feed.append(unordered[key])
 	return feed
 
-def do_work(outfilename):
-	datespec = time.strftime("%d-%b-%Y", time.gmtime(time.mktime(time.gmtime())-86400))
+def do_work():
+	datespec = time.strftime("%d-%b-%Y", time.gmtime(time.mktime(time.gmtime()) - long(CONFIG['DAYS']) * 86400))
 	feed = ET.Element("feed")
 	feed.attrib['version'] = "0.3"
 	feed.attrib['xmlns'] = "http://purl.org/atom/ns#"
-	ET.SubElement(feed, "title").text = "IMAP %s@%s" % (IMAPUSERNAME, IMAPSERVER)
+	ET.SubElement(feed, "title").text = "IMAP %s@%s" % (CONFIG['IMAPUSERNAME'], CONFIG['IMAPSERVER'])
 	ET.SubElement(feed, "updated").text = "%s" % (time.strftime("%Y-%m-%dT%H:%M:%SZ"))
 	link = ET.SubElement(feed, "link")
 	link.attrib['rel'] = "alternate"
-	link.attrib['href'] = "imap://%s/%s" % (IMAPSERVER, "INBOX")
+	link.attrib['href'] = "imap://%s/%s" % (CONFIG['IMAPSERVER'], CONFIG['FOLDERNAME'])
 	generate_feed(feed, gethdrs("(SINCE %s)" % datespec))
-	file(outfilename, "w").write(ET.tostring(feed))
+	file(CONFIG['OUTPUTFILE'], "w").write(ET.tostring(feed))
 
 if __name__ == "__main__":
 	import sys
-	if len(sys.argv) < 2:
-		print "Usage: imap2atom.py filename"
+	if len(sys.argv) != 1:
+		print "Usage: imap2atom.py"
 		sys.exit()
-	do_work(sys.argv[1])
+	RCFILE = os.path.join(os.environ['HOME'], ".imap2atomrc")
+	if os.path.exists(RCFILE):
+		readconfig(RCFILE)
+	if CONFIG['DEBUG']:
+		print "Resources: %s" % str(CONFIG)
+	CONFIG['IMAPPASSWORD'] = getpass.getpass()
+	do_work()
